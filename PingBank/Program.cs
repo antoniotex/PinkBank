1e4dfd89102b1d511879afd3da80da6d2927e747
antoniotex <acm.skt@gmail.com>
2018-10-07 15:57:50 -0300

    Forçado erro de null reference

--- PingBank/Program.cs
+++ PingBank/Program.cs
@@ -10,19 +10,19 @@ namespace PingBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
-            gabriela.nome = "Gabriela";
-            gabriela.profissao = "Dev C#";
-            gabriela.cpf = "15036981731";
+            // Cliente gabriela = new Cliente();
+            // gabriela.nome = "Gabriela";
+            // gabriela.profissao = "Dev C#";
+            // gabriela.cpf = "15036981731";
 
             ContaCorrente conta = new ContaCorrente();
 
-            conta.titular = gabriela;
+            // conta.titular = gabriela;
             conta.saldo = 500;
             conta.agencia = 563;
             conta.numero = 563452;
 
-            Console.WriteLine(gabriela.nome);
+            // Console.WriteLine(gabriela.nome);
             Console.WriteLine(conta.titular.nome);
 
             Console.ReadLine();
