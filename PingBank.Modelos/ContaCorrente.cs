14a93893afcbf397c292e208aaaec2788454bace
antoniotex <acm.skt@gmail.com>
2018-10-31 23:45:44 -0300

    new project

---
+++ PingBank.Modelos/ContaCorrente.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace PingBank
+{
+    /// <summary>
+    /// Define uma Conta Corrente do banco PingBank
+    /// </summary>
+    public class ContaCorrente
+    {
+        public Cliente Titular { get; set; }
+
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        public static double TaxaOperacao { get; private set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public int Agencia { get; }
+   
+        public int Numero { get; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        /// <summary>
+        /// Cria uma instância de ContaCorrente com os argumentos utilizados
+        /// </summary>
+        /// <param name="agencia">Representa o valor da propriedade <see cref="Agencia"/> e deve possuir um valor maior que zero</param>
+        /// <param name="numero">Representa o valor da propriedade <see cref="Numero"/> e deve possuir um valor maior que zero</param>
+        public ContaCorrente(int agencia, int numero)
+        {
+            if(agencia <= 0 || numero <= 0)
+            {
+
+                //nameof() é um operador do CSharp que transforma em string o nome da variavel (Boa prática)
+                //Dessa forma abaixo estou passando como argumento a Message e o ParamName
+                ArgumentException excecao = new ArgumentException("A ag e numero devem ser maiores que zero", nameof(numero));
+
+                throw excecao;
+
+                //throw new Exception("A ag e numero devem ser maiores que zero");
+                //Console.WriteLine(Titular.Nome);
+                //Lançando uma excessão
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        public void Sacar(double valor)
+        {
+            if(valor < 0)
+            {
+                throw new ArgumentException("Valor invalido para o saque", nameof(valor));
+            }
+
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor invalido para a transferencia" + nameof(valor));
+            }
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operação não realizada", ex);
+            }
+            contaDestino.Depositar(valor);            
+        }
+
+    }
+}
---
+++ PingBank.Modelos/SaldoInsuficienteException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingBank
+{
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(double saldo, double valorSaque)
+            : this("Tentativa de saque no valor de " + valorSaque + ", em uma conta com o valor de " + saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
+
+        public SaldoInsuficienteException(string msg)
+            : base(msg)
+        {
+        }
+
+        public SaldoInsuficienteException(string mensagem, Exception excecaoInterna)
+            : base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
