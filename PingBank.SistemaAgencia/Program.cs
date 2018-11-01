00bc152c34b957a7bbe399c02a7335ee16f125a9
antoniotex <acm.skt@gmail.com>
2018-11-01 00:16:53 -0300

    Começando a trabalhar com datas (TimeSpan, DateTime)

--- PingBank.SistemaAgencia/Program.cs
+++ PingBank.SistemaAgencia/Program.cs
@@ -11,9 +11,29 @@ namespace PingBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(487, 487102);
+            DateTime dataFimPagamento = new DateTime(2019, 12, 02);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + GetIntervaloDeTempoLegivel(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.WriteLine(dataFimPagamento);
 
             Console.ReadLine();
+        }
+
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            if(timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+                return quantidadeMeses + " meses";
+            }
+
+            return timeSpan.Days + " dias";
         }
     }
 }
